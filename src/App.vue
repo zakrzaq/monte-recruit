7f6ef889af0a89bd7215d5761e457c68f9d955a5
Jakub Zakrzewski <j.zakrzewsky@gmail.com>
2022-07-15 13:40:49 +0200

    cleanup

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,14 @@
 <template>
   <main class="main-wrapper">
     <h1>hello</h1>
-    <BaseInput inp-type="text" place-text="something ending with monterail.com">Email</BaseInput>
-    <BaseInput inp-type="password" place-text="*****">Password</BaseInput>
-
     <router-view></router-view>
   </main>
 </template>
 
 <script>
-import BaseInput from './components/base/BaseInput.vue'
-
 export default {
   name: 'App',
   components: {
-    BaseInput,
   }
 }
 </script>
