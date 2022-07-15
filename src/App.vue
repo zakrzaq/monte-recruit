ee61c8f7e85b9dc5b6c8e5de01b1f250790cc908
Jakub Zakrzewski <j.zakrzewsky@gmail.com>
2022-07-15 10:53:41 +0200

    cleanup

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,14 @@
 <template>
-  <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
-    <router-view/>
-  </div>
+
+  <main class="main-wrapper">
+    <h1>hello</h1>
+    <router-view></router-view>
+  </main>
 </template>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+.main-wrapper {
+  max-width: 900px;
+  margin: 0 auto;
 }
 </style>
---
+++ src/views/RegisterView.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    register view
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'RegisterView'
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
