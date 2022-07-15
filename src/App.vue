90a40109d3d7670d04851f8c03ed30b91ae0db1e
Jakub Zakrzewski <j.zakrzewsky@gmail.com>
2022-07-15 11:27:43 +0200

    post fonts

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,20 @@
 <template>
   <main class="main-wrapper">
     <h1>hello</h1>
+    <BaseButton>Button</BaseButton>
     <router-view></router-view>
   </main>
 </template>
+
+<script>
+import BaseButton from './components/base/BaseButton.vue'
+export default {
+  name: 'App',
+  components: {
+    BaseButton
+  }
+}
+</script>
 
 <style lang="scss">
 .main-wrapper {
---
+++ src/components/base/BaseButton.vue
@@ -0,0 +1,34 @@
+<template>
+  <button class="btn">
+    <slot />
+  </button>
+</template>
+
+<script>
+export default {
+  name: 'BaseButton'
+}
+</script>
+
+<style lang="scss" scoped>
+.btn {
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  padding: 12px 24px;
+  gap: 10px;
+  background: $primary;
+  color: $white;
+  border-radius: 64px;
+  border: none;
+  flex: none;
+  order: 1;
+  flex-grow: 0;
+  font-family: 'Roboto Mono';
+  font-style: normal;
+  font-weight: 500;
+  font-size: 16px;
+  line-height: 100%;
+}
+</style>
